e9ad46b2fdae4d7fe1f879f2c56a0c2923a1bf9e
Alan <103587817+Lumberj3ck@users.noreply.github.com>
2025-02-21 14:17:30 +0100

    added zig, npm for nvim

--- main.sh
+++ main.sh
@@ -1,20 +1,30 @@
+echo "Fixing winget unziping" 
+cp winget_settings.json "C:\Users\alyulda\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState"
 
+
+echo "Installing python"
 winget install Python.Python.3.13
 
+
+echo "Installing neovim"
 winget install Neovim.Neovim --accept-package-agreements   --disable-interactivity
 
+echo "Installing clink"
 winget install clink
 
-winget install starship
 
+echo "Installing gnu for windows"
 winget install bmatzelle.Gow
-
-
-nvim_conf=$USERPROFILE\AppData\Local\nvim
-mkdir $nvim_conf
-
-
-git clone https://github.com/Lumberj3ck/my_nvim_config.git $nvim_conf
-
-
+echo "Installing starship"
+winget install starship
+cp starship.lua "C:\Program Files (x86)\clink"
 winget install --id Docker.DockerDesktop
+winget install -e --id zig.zig
+winget install OpenJS.NodeJS
+nvim_conf="$USERPROFILE\AppData\Local\nvim"
+mkdir $nvim_conf
+git clone -b heavy_config https://github.com/Lumberj3ck/my_nvim_config.git $nvim_conf
+
+
+
+# curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip
